96b3e1fd331b40fae3cf5905164d9e6687781977
anjanroushan <35198037+anjanroushan@users.noreply.github.com>
2018-03-19 23:45:09 +0530

    first commit

--- Selenium/src/SeleniumBasic.java
+++ Selenium/src/SeleniumBasic.java
@@ -2,6 +2,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class SeleniumBasic {
+	
 
 	public static void main(String[] args)  {
 		System.setProperty("webdriver.gecko.driver", "G:\\Selenium jars2.42\\geckodriver-v0.16.1-win64\\geckodriver.exe");
